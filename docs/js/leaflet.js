f3d19dab33090c40658435bb64ec65dbe8376279
Chris Ward <cward@redhat.com>
2016-09-08 00:07:33 +0200

    rearrange sections; recenter map; add empty footer

--- docs/js/leaflet.js
+++ docs/js/leaflet.js
@@ -4,7 +4,7 @@ jQuery(document).ready(main)
 
 function main() {
   var mymap = L.map('venue-map', {
-    center: [49.2268484, 16.5964398],
+    center: [49.2273, 16.5964398],
     zoom: 17,
   });
 
@@ -23,6 +23,15 @@ function main() {
     .setLatLng([49.2268484, 16.5964398])
     .setContent("<b><center>Faculty of Information Technology</b></br>Brno University of Technology (VUT FIT).<br/><br/><i>Božetěchova 1/2, <br/>612 66 Královo Pole, Brno</i><br/><br/><a href='https://www.google.cz/maps/place/Faculty+of+Information+Technology+Brno+University+of+Technology,+Bo%C5%BEet%C4%9Bchova+1%2F2,+612+66+Brno-Kr%C3%A1lovo+Pole/@49.226851,16.5945971,17z/data=!3m1!4b1!4m5!3m4!1s0x471294099f6249fb:0x46a6dce891d1b33d!8m2!3d49.226851!4d16.5967858' target='_blank'>( <i class='fa fa-map-o' aria-hidden='true'></i> map )</a></center>")
     .openOn(mymap);
+
+  mymap.touchZoom.disable();
+  mymap.doubleClickZoom.disable();
+  mymap.scrollWheelZoom.disable();
+  mymap.boxZoom.disable();
+  mymap.keyboard.disable();
+//  mymap.dragging.disable();
+//  click on the map makes the popup dissapear
+  $(".leaflet-control-zoom").css("visibility", "hidden");
 
 }
 
