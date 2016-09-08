aedaff1163abb625aacf161e446bb9e1ada4e387
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2016-09-08 10:34:53 +0200

    Merge fix, robust navbar js

--- docs/js/devconf.js
+++ docs/js/devconf.js
@@ -1,7 +1,6 @@
 (function ($) {
   $(document).ready(function(){
 
-<<<<<<< HEAD
   // hide .navbar first
   $(".navbar").hide();
 
@@ -16,23 +15,10 @@
   }
 
   // hook on scroll and show/hide navbar
-=======
-  // FIXME: if page is refreshed while lower at the bottom of the page the navbar does appear...
-  // eg, load page, scroll down (navbar appears), CTRL-R... navbar remains hidden even though
-  // we are far down the page
-  // hide .navbar first
-  $(".navbar").hide();
-
-  // fade in .navbar
->>>>>>> 5d752f56334ffc788db5b0c23cba9180680178f3
   $(function () {
     $(window).scroll(function () {
       // set distance user needs to scroll before we fadeIn navbar
       // FIXME: Make this START at the ABOUT page
-<<<<<<< HEAD
-=======
-      logo_pos = $("#devconf-logo").offset().top - $("#page-top").offset().top
->>>>>>> 5d752f56334ffc788db5b0c23cba9180680178f3
       if ($(this).scrollTop() > logo_pos) {
         $('.navbar').fadeIn();
       } else {
