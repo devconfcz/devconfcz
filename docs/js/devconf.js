9303b8f3f3a2652686510a4784aa91a5a9f3a4c6
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2016-09-08 15:26:13 +0200

    Always leave navbar visible

--- docs/js/devconf.js
+++ docs/js/devconf.js
@@ -1,17 +1,14 @@
 (function ($) {
   $(document).ready(function(){
 
-  // hide .navbar first
-  $(".navbar").hide();
-
-  // get logo distance from top
+  // get distance from top of the bottom logo border
   logo_pos = $("#devconf-logo").offset().top - $("#page-top").offset().top
 
   // user can load already scrolled page (eg. refreshing, ...); check it even on document ready
   if ($(this).scrollTop() > logo_pos) {
-    $('.navbar').fadeIn();
+    $('.navbar-brand').fadeIn();
   } else {
-    $('.navbar').fadeOut();
+    $('.navbar-brand').fadeOut();
   }
 
   // hook on scroll and show/hide navbar
@@ -20,9 +17,9 @@
       // set distance user needs to scroll before we fadeIn navbar
       // FIXME: Make this START at the ABOUT page
       if ($(this).scrollTop() > logo_pos) {
-        $('.navbar').fadeIn();
+        $('.navbar-brand').fadeIn();
       } else {
-        $('.navbar').fadeOut();
+        $('.navbar-brand').fadeOut();
       }
     });
 
