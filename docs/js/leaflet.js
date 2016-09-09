f6407991ffb9a12808e41fde8d6f0c812d9ee13a
Chris Ward <cward@redhat.com>
2016-09-09 12:48:23 +0200

    merge @lukassparrow PR#16 with master

--- docs/js/leaflet.js
+++ docs/js/leaflet.js
@@ -21,23 +21,24 @@ function main() {
   var marker = L.marker([49.2269, 16.5954]).addTo(mymap);
   var popup = L.popup()
     .setLatLng([49.2269, 16.5954])
-    .setContent("<b><center>Faculty of Information Technology</b></br>Brno University of Technology (VUT FIT)<br/><br/><i>Božetěchova 1/2 <br/>612 66 Královo Pole, Brno</i><br/><br/><a href='https://www.google.cz/maps/place/Faculty+of+Information+Technology+Brno+University+of+Technology,+Bo%C5%BEet%C4%9Bchova+1%2F2,+612+66+Brno-Kr%C3%A1lovo+Pole/@49.226851,16.5945971,17z/data=!3m1!4b1!4m5!3m4!1s0x471294099f6249fb:0x46a6dce891d1b33d!8m2!3d49.226851!4d16.5967858' target='_blank'>( <i class='fa fa-map-o' aria-hidden='true'></i> map )</a></center>")
+    .setContent("<div style='font-size: .7em'><b><center>Faculty of Information Technology</b></br>Brno University of Technology (VUT FIT)<br/><br/><i>Božetěchova 1/2 <br/>612 66 Královo Pole, Brno</i><br/><br/><a href='https://www.google.cz/maps/place/Faculty+of+Information+Technology+Brno+University+of+Technology,+Bo%C5%BEet%C4%9Bchova+1%2F2,+612+66+Brno-Kr%C3%A1lovo+Pole/@49.226851,16.5945971,17z/data=!3m1!4b1!4m5!3m4!1s0x471294099f6249fb:0x46a6dce891d1b33d!8m2!3d49.226851!4d16.5967858' target='_blank'>( <i class='fa fa-map-o' aria-hidden='true'></i> map )</a></center></div>")
     .openOn(mymap);
 
   mymap.touchZoom.disable();
-  mymap.doubleClickZoom.disable();
+//  mymap.doubleClickZoom.disable();
   mymap.scrollWheelZoom.disable();
   mymap.boxZoom.disable();
   mymap.keyboard.disable();
-//  mymap.dragging.disable();
+  mymap.dragging.disable();
 //  click on the map makes the popup dissapear
-  $(".leaflet-control-zoom").css("visibility", "hidden");
+//  $(".leaflet-control-zoom").css("visibility", "hidden");
 
   $("section").not("#venue").click(function(){
     mymap.scrollWheelZoom.disable();
   });
 
   mymap.on('click', function() {
+//    mymap.dragging.enable();
     mymap.scrollWheelZoom.enable();
   });
 
