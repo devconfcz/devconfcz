1530df2ebc19828c0edbfb2c6a7d69da2ed7cf01
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2016-11-10 16:56:12 +0100

    Import mor travel information

--- docs/js/devconf.js
+++ docs/js/devconf.js
@@ -8,30 +8,46 @@ function scroll_to_section(section /* Pass element id with # */) {
 
 (function ($) {
   $(document).ready(function(){
-
-  // get distance from top of the bottom logo border
-  logo_pos = $("#devconf-logo").offset().top - $("#page-top").offset().top
-
-  // user can load already scrolled page (eg. refreshing, ...); check it even on document ready
-  if ($(this).scrollTop() > logo_pos) {
-    $('.navbar-brand').fadeIn();
-  } else {
-    $('.navbar-brand').fadeOut();
-  }
-
-  // hook on scroll and show/hide navbar
-  $(function () {
-    $(window).scroll(function () {
-      // Keep amount of things here at minimum to keep it smooth on low-end devices
-      if ($(this).scrollTop() > logo_pos) {
-        $('.navbar-brand').fadeIn();
-      } else {
-        $('.navbar-brand').fadeOut();
-      }
+    var travel_height = $('#travel-text')[0].scrollHeight;
+    $('#more-text').click(function(e) {
+        e.stopPropagation();
+        $('#travel-text').animate({
+            'height': travel_height
+        })
+        $('#more-text').css('visibility', 'hidden')
+        $('#less-text').css('visibility', 'visible')
     });
+    $('#less-text').click(function() {
+        $('#travel-text').animate({
+            'height': '400px'
+        })
+        $('#more-text').css('visibility', 'visible')
+        $('#less-text').css('visibility', 'hidden')
+    })
+
+    // get distance from top of the bottom logo border
+    logo_pos = $("#devconf-logo").offset().top - $("#page-top").offset().top
+
+    // user can load already scrolled page (eg. refreshing, ...); check it even on document ready
+    if ($(this).scrollTop() > logo_pos) {
+      $('.navbar-brand').fadeIn();
+    } else {
+      $('.navbar-brand').fadeOut();
+    }
+
+    // hook on scroll and show/hide navbar
+    $(function () {
+      $(window).scroll(function () {
+        // Keep amount of things here at minimum to keep it smooth on low-end devices
+        if ($(this).scrollTop() > logo_pos) {
+          $('.navbar-brand').fadeIn();
+        } else {
+          $('.navbar-brand').fadeOut();
+        }
+      });
 
 
-  });
+    });
 
 });
   }(jQuery));
